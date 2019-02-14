80acec0bee2278c201bff85e0814cc260659f1a3
Johan Larsson <joh.larsson@cgi.com>
2019-02-14 19:48:37 +0100

    faultfinding env variables

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ export default class App extends Component {
   componentDidMount() {
     // Fetch all todos
     api.readAll().then((todos) => {
+      console.warn(process.env)
       if (todos.message === 'unauthorized') {
         if (isLocalHost()) {
           alert('FaunaDB key is not unauthorized. Make sure you set it in terminal session where you ran `npm start`. Visit http://bit.ly/set-fauna-key for more info')
